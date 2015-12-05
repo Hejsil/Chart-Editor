4be3a2f8f36b5973b6ee268f1b49bfcffc35f09e
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2015-12-05 15:46:07 +0100

    new NoteMapController

---
+++ NoteMapControllers/TempNote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using NoteMapModel;
+
+namespace NoteMapControllers
+{
+    public class TempNote : IComparable<TempNote>
+    {
+        public int Position { get; set; }
+        public int Length { get; set; }
+        public byte Note { get; set; }
+        public List<int> MappingList { get; set; }
+
+        public TempNote(int pos, int length, byte note, List<int> mapping)
+        {
+            Position = pos;
+            Length = length;
+            Note = note;
+            MappingList = mapping;
+        }
+
+        public TrackEvent ToNote()
+        {
+            return new TrackEvent(Position, (TrackEventType)MappingList.IndexOf(Note), Length);
+        }
+
+        public int CompareTo(TempNote other)
+        {
+            return Position - other.Position;
+        }
+    }
+}
---
+++ NoteMapModel/NoteMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteMapModel
+{
+    public class NoteMap : List<Track>
+	{
+		#region Fields
+		public int Offset { get; set; }
+		public int Division { get; private set; }
+
+		public List<MetaEvent> MetaEvents { get; private set; }
+		#endregion
+
+		#region Constructors
+		public NoteMap(int division = 64, int offset = 0)
+		{
+			Division = division;
+			Offset = offset;
+			MetaEvents = new List<MetaEvent>();
+        }
+		#endregion
+
+		#region Public Methods
+
+		#endregion
+
+		#region Private Methods
+
+		#endregion
+	}
+}
